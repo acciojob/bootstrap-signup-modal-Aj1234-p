404c4703671dcd1037a44904260fb3df487356f4
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-06-25 09:26:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-//your JS code here. If required.
-const button = document.querySelector("#btn");
-const forms = document.querySelectorAll('.needs-validation')
-button.addEventListener('click',()=>{
-	forms.classList.remove("d-none");
-})
+//your JS code here. If required
